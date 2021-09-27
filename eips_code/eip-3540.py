b5789facfc62b9ef1b2dd0e60da13af10636f16f
Andrei Maiboroda <andrei@ethereum.org>
2021-09-27 19:59:26 +0200

    Add python code from EIP-3540 with tests

---
+++ eips_code/eip-3540.py
@@ -0,0 +1,90 @@
+FORMAT = 0xEF
+MAGIC = 0x00
+VERSION = 0x01
+S_TERMINATOR = 0x00
+S_CODE = 0x01
+S_DATA = 0x02
+
+
+def validate_eof(code: bytes):
+    # Old-style contracts are still allowed
+    if len(code) == 0 or code[0] != FORMAT:
+        return
+
+    # Validate format and magic
+    assert(len(code) >= 3 and code[1] == MAGIC and code[2] == VERSION)
+
+    # Process section headers
+    section_sizes = {S_CODE: 0, S_DATA: 0}
+    pos = 3
+    while True:
+        # Terminator not found
+        assert(pos < len(code))
+        section_id = code[pos]
+        pos += 1
+        if section_id == S_TERMINATOR:
+            break
+
+        # Disallow unknown sections
+        assert(section_id in section_sizes)
+
+        # Data section preceding code section
+        assert(not (section_id == S_DATA and section_sizes[S_CODE] == 0))
+
+        # Multiple sections with the same id
+        assert(section_sizes[section_id] == 0)
+
+        # Truncated section size
+        assert((pos + 1) < len(code))
+        section_sizes[section_id] = (code[pos] << 8) | code[pos + 1]
+        pos += 2
+
+        # Empty section
+        assert(section_sizes[section_id] != 0)
+
+    # Code section cannot be absent
+    assert(section_sizes[S_CODE] != 0)
+
+    # The entire container must be scanned
+    assert(len(code) == (pos + section_sizes[S_CODE] + section_sizes[S_DATA]))
+
+def validate_code(code: bytes) -> bool:
+    try:
+        validate_eof(code)
+        return True
+    except:
+       return False
+
+# Legacy contracts
+assert validate_code(b'') == True
+assert validate_code(b'\x00') == True
+# No magic
+assert validate_code(b'\xef') == False
+# Any value outside the magic
+for magic in range(1, 256):
+    assert validate_code(b'\xef' + bytes([magic])) == False
+# EOF1 contracts
+assert validate_code(b'\xef\x00') == False # Only magic
+assert validate_code(b'\xef\x00\x01') == False # Only version
+assert validate_code(b'\xef\x00\x00') == False # Wrong version
+assert validate_code(b'\xef\x00\x02\x01\x00\x01\x00\xfe') == False # Valid except version
+assert validate_code(b'\xef\x00\x01\x00') == False # Only terminator
+assert validate_code(b'\xef\x00\x01\x00\x000') == False # Trailing bytes
+assert validate_code(b'\xef\x00\x01\x01') == False # Truncated section header
+assert validate_code(b'\xef\x00\x01\x02') == False # Truncated section header
+assert validate_code(b'\xef\x00\x01\x01\x00\x01\x02') == False # Truncated section header
+assert validate_code(b'\xef\x00\x01\x03') == False # Invalid section id
+assert validate_code(b'\xef\x00\x01\x01\x00') == False # Truncated code section size
+assert validate_code(b'\xef\x00\x01\x01\x00\x01\x02\x00') == False # Truncated data section size
+assert validate_code(b'\xef\x00\x01\x01\x00\x00\x00') == False # Empty code section size
+assert validate_code(b'\xef\x00\x01\x01\x00\x01\x02\x00\x00\x00\xfe') == False # Empty data section
+assert validate_code(b'\xef\x00\x01\x01\x00\x01') == False # No terminator after section
+assert validate_code(b'\xef\x00\x01\x01\x00\x01\x00') == False # Missing section contents
+assert validate_code(b'\xef\x00\x01\x02\x00\x01\x00\xaa') == False # Only data section
+assert validate_code(b'\xef\x00\x01\x01\x00\x01\x01\x00\x01\x00\xfe\xfe') == False # Multiple code sections
+assert validate_code(b'\xef\x00\x01\x01\x00\x01\x02\x00\x01\x02\x00\x01\x00\xfe\xaa\xbb') == False # Multiple data sections
+assert validate_code(b'\xef\x00\x01\x01\x00\x01\x01\x00\x01\x02\x00\x01\x02\x00\x01\x00\xfe\xfe\xaa\xbb') == False # Multiple code and data sections
+assert validate_code(b'\xef\x00\x01\x02\x00\x01\x01\x00\x01\x00\xaa\xfe') == False # Data section before code section
+
+assert validate_code(b'\xef\x00\x01\x01\x00\x01\x00\xfe') == True # Valid format with 1-byte of code
+assert validate_code(b'\xef\x00\x01\x01\x00\x01\x02\x00\x01\x00\xfe\xaa') == True # Code and data section
