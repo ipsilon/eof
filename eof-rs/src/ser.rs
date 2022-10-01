54fe924038885e5ec1cb557c3355e5d1bd91c894
Alex Beregszaszi <alex@rtfs.hu>
2022-10-01 14:29:43 +0200

    f

--- eof-rs/src/ser.rs
+++ eof-rs/src/ser.rs
@@ -350,51 +350,61 @@ struct HeaderEntry {
 }
 
 struct Encoder {
+    version: u8,
     headers: Vec<HeaderEntry>,
     contents: Vec<Vec<u8>>,
 }
 
 impl Encoder {
-//   pub fn push_section(&mut self, data: &[u8]) -> Result<()> {
-  // }
+    pub fn push_section(&mut self, section: EOFSection) -> Result<()> {
+        let section_kind = section.kind();
 
-
-   pub fn push_section(&mut self, section: EOFSection) -> Result<()> {
-     let section_kind = section.kind();
-   
-     // Encode content
-    let content = match section {
+        // Encode content
+        let content = match section {
             EOFSection::Code(code) => code,
             EOFSection::Data(data) => data,
             EOFSection::Type(types) => unimplemented!(),
             _ => unimplemented!(),
-     };
-     let content_len = content.len();
-     self.contents.push(content);
+        };
+        let content_len = content.len();
+        self.contents.push(content);
 
-     // Store header
-     self.headers.push(HeaderEntry {
-       kind: section_kind,
-       size: content_len as u16,
-//       size: u16::try_from(content.len())?
-     });
-     unimplemented!()
-   }
+        // Store header
+        self.headers.push(HeaderEntry {
+            kind: section_kind,
+            size: content_len as u16,
+            //       size: u16::try_from(content.len())?
+        });
 
-   pub fn finalize(&mut self) -> Result<Vec<u8>> {
-     let mut encoded_headers = self.headers.iter().map(|header| vec![header.kind, (header.size >> 8) as u8, (header.size & 0xff) as u8]).flatten().collect();
-     let mut encoded_contents: Vec<u8> = self.contents.iter().flatten().collect();
+        Ok(())
+    }
 
-     let mut ret = Vec::<u8>::new();
-     ret.append(&mut encoded_headers);
-     ret.push(0); // terminator
+    pub fn finalize(&mut self) -> Result<Vec<u8>> {
+        let mut encoded_headers = self
+            .headers
+            .iter()
+            .map(|header| {
+                vec![
+                    header.kind,
+                    (header.size >> 8) as u8,
+                    (header.size & 0xff) as u8,
+                ]
+            })
+            .flatten()
+            .collect();
+        let mut encoded_contents: Vec<u8> = vec![]; //self.contents.iter().flatten().collect();
 
-     Ok(ret)
-   }
+        let mut ret = vec![0xef, 0x00, self.version];
+        ret.append(&mut encoded_headers);
+        ret.push(0); // terminator
+        ret.append(&mut encoded_contents);
+
+        Ok(ret)
+    }
 }
 
 fn encode_types(types: Vec<EOFTypeSectionEntry>) -> Vec<u8> {
-   unimplemented!()
+    unimplemented!()
 }
 
 pub fn to_bytes(value: EOFContainer) -> Result<Vec<u8>> {
