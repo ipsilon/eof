366f461697a2287c1f79227e344e9e47b1391a34
Andrei Maiboroda <andrei@ethereum.org>
2022-11-16 12:29:59 -0600

    Refactor and use EOF class
    Co-authored-by: Paweł Bylica <pawel@hepcolgum.band>

--- eips_code/eof1_validation.py
+++ eips_code/eof1_validation.py
@@ -1,29 +1,25 @@
+from dataclasses import dataclass
+
 from eip4750 import *
 from eip5450 import validate_function, FunctionType
+
+
+@dataclass
+class EOF:
+    types: list[FunctionType]
+    codes: list[bytes]
 
 
 # EOF code validation, including container format validation, code section validation and function stack validation.
 # Raises ValidationException on invalid code
 def validate_eof1(container: bytes):
     validate_eof(container)
-
-    section_sizes, header_size = read_eof1_header(container)
-
-    if len(section_sizes[S_TYPE]) != 0:
-        type_section_size = section_sizes[S_TYPE][0]
-        types = read_eof1_types(container, header_size, type_section_size)
-        pos = header_size + type_section_size
-    else:
-        types = [FunctionType(0, 0)]  # implicit code section 0 type
-        pos = header_size
-
-    for func_idx in range(0, len(section_sizes[S_CODE])):
-        code_section_size = section_sizes[S_CODE][func_idx]
-        validate_function(func_idx, container[pos:pos + code_section_size], types)
-        pos += code_section_size
+    eof = read_eof1_header(container)
+    for func_idx, code in enumerate(eof.codes):
+        validate_function(func_idx, code, eof.types)
 
 
-def read_eof1_header(container: bytes) -> (dict[int, list[int]], int):
+def read_eof1_header(container: bytes) -> EOF:
     section_sizes = {S_TYPE: [], S_CODE: [], S_DATA: []}
     pos = len(MAGIC) + 1
     while True:
@@ -35,7 +31,19 @@ def read_eof1_header(container: bytes) -> (dict[int, list[int]], int):
         section_sizes[section_id].append((container[pos] << 8) | container[pos + 1])
         pos += 2
 
-    return section_sizes, pos
+    if len(section_sizes[S_TYPE]) != 0:
+        type_section_size = section_sizes[S_TYPE][0]
+        types = read_eof1_types(container, pos, type_section_size)
+        pos += type_section_size
+    else:
+        types = [FunctionType(0, 0)]  # implicit code section 0 type
+
+    eof = EOF(types, [])
+    for code_size in section_sizes[S_CODE]:
+        eof.codes.append(container[pos:pos + code_size])
+        pos += code_size
+
+    return eof
 
 
 def read_eof1_types(container: bytes, header_size: int, type_section_size: int) -> list[FunctionType]:
