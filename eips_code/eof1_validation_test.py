25286a6b117fea16de4453485da8e46e9b01cd9b
Andrei Maiboroda <andrei@ethereum.org>
2022-11-18 12:19:29 +0100

    Change CALLF/RETF opcodes in eof1_validaton_test

--- eips_code/eof1_validation_test.py
+++ eips_code/eof1_validation_test.py
@@ -21,14 +21,14 @@ def test_valid_eof1_container():
     # Code section and data section
     assert is_valid_eof(bytes.fromhex("ef000101000102000100feaa"))
     # Type section and two code sections
-    assert is_valid_eof(bytes.fromhex("ef0001 030004 010001 010003 00 00000001 fe 6000fc"))
+    assert is_valid_eof(bytes.fromhex("ef0001 030004 010001 010003 00 00000001 fe 6000b1"))
     # Type section, two code sections, data section
-    assert is_valid_eof(bytes.fromhex("ef0001 030004 010001 010002 020004 00 00000201 fe 50fc aabbccdd"))
+    assert is_valid_eof(bytes.fromhex("ef0001 030004 010001 010002 020004 00 00000201 fe 50b1 aabbccdd"))
 
     # Example with 3 functions
     assert is_valid_eof(bytes.fromhex("ef0001 030006 01003b 010017 01001d 00 000001010101 "
-                                      "60043560e06000351c639fb890d581145d001c6320cb776181145d00065050600080fd50fb000260005260206000f350fb000160005260206000f3"
-                                      "600181115d0004506001fc60018103fb000281029050fc 600281115d0004506001fc60028103fb000160018203fb0001019050fc"))
+                                      "60043560e06000351c639b0890d581145d001c6320cb776181145d00065050600080fd50b0000260005260206000f350b0000160005260206000f3"
+                                      "600181115d0004506001b160018103b0000281029050b1 600281115d0004506001b160028103b0000160018203b00001019050b1"))
 
 
 def test_invalid_eof1_container():
@@ -39,6 +39,6 @@ def test_invalid_eof1_container():
     # EIP-4200 violation - invalid RJUMP
     is_invalid_eof_with_error(bytes.fromhex("ef0001 010004 00 5c00ff00"), "relative jump destination out of bounds")
     # EIP-4750 violation - invalid CALLF
-    is_invalid_eof_with_error(bytes.fromhex("ef0001 030004 010005 010003 00 00000001 fbffff5000 6000fc"), "invalid section id")
+    is_invalid_eof_with_error(bytes.fromhex("ef0001 030004 010005 010003 00 00000001 b0ffff5000 6000b1"), "invalid section id")
     # EIP-5450 violation - stack underflow
-    is_invalid_eof_with_error(bytes.fromhex("ef0001 030004 010005 010004 00 00000001 fb00015000 600001fc"), "stack underflow")
+    is_invalid_eof_with_error(bytes.fromhex("ef0001 030004 010005 010004 00 00000001 b000015000 600001b1"), "stack underflow")
